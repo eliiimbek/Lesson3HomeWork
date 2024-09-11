165ba8a8c9cda0ce14fb4ece7a5c35d1e1c6ad25
eliiimbek <elimbek.work49@gmail.com>
2024-09-12 00:18:37 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+// Задание №1
+// function minNumber(a, b) {
+//     return Math.min(a, b);
+// }
+//
+// console.log(minNumber(3, 6));
+// console.log(minNumber(3, 1));
+//
+// // Задание №2
+//
+// function getStringLength() {
+//     let userInput = prompt("Введите строку:");
+//     console.log(userInput.length);
+// }
+//
+// getStringLength();
+
+// Задание №3
+
+// function calculator(num1, num2, operator) {
+//     switch(operator) {
+//         case "+":
+//             return num1 + num2;
+//         case "-":
+//             return num1 - num2;
+//         case "*":
+//             return num1 * num2;
+//         case "/":
+//             if (num2 !== 0) {
+//                 return num1 / num2;
+//             } else {
+//                 return "Деление на ноль невозможно!";
+//             }
+//         default:
+//             return "Неверный оператор!";
+//     }
+// }
+//
+// console.log(calculator(10, 5, "/"));
+
+
